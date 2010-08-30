8f528692827e20a15208336044e0adfe646d3cdd
Noah K. Tilton <binaural@gmail.com>
2010-08-29 23:30:57 -0500

    rtorrent status file

---
+++ rtorrent_status.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+self=$(basename $0)
+rtorrent_pid=$(ps fuax|grep rtorrent|grep -v "grep\|$self")
+
+if [[ -z "$rtorrent_pid" ]]; then
+  echo "not running"
+else
+  echo "pid $rtorrent_pid"
+fi
